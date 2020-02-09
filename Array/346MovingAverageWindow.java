339f31495f91ea0a704989d906f667cbd94b31bb
Yuexi-Wu <53302295+Yuexi-Wu@users.noreply.github.com>
2020-02-09 13:56:47 -0500

    Update 346. Moving Average from Data Stream

--- Array/346MovingAverageWindow.java
+++ Array/346MovingAverageWindow.java
@@ -33,3 +33,31 @@ class MovingAverage {
  * MovingAverage obj = new MovingAverage(size);
  * double param_1 = obj.next(val);
  */
+class MovingAverage {
+
+    int[] array;
+    int size = 0;
+    int counter = 0;
+    int head = 0, tail = 0;
+    int sum = 0;
+    /** Initialize your data structure here. */
+    public MovingAverage(int size) {
+        array = new int[size];
+        this.size = size;
+    }
+    
+    public double next(int val) {
+        counter++;
+        int tail = (head+1) % size;
+        sum  = sum + val - array[tail];
+        head = tail;
+        array[head] = val;
+        return sum * 1.0 / Math.min(size, counter);
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
