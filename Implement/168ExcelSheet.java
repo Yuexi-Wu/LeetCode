e4f708932d8de48aefe0fce592191e54b9ddb7d8
Yuexi-Wu <53302295+Yuexi-Wu@users.noreply.github.com>
2020-02-11 10:45:54 -0500

    Create 168. Excel Sheet Column Title

---
+++ Implement/168ExcelSheet.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            n--;
+            sb.append((char)(n % 26 + 65));
+            n = n / 26;
+        }
+        return sb.reverse().toString();
+    }
+}
