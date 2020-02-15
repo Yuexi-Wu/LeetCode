a9b72dbcd169c449519337b9d0182208a1f480cb
Yuexi-Wu <53302295+Yuexi-Wu@users.noreply.github.com>
2020-02-15 13:00:32 -0500

    Update 46. Permutations

--- Recurse/46Permutations.java
+++ Recurse/46Permutations.java
@@ -24,3 +24,35 @@ class Solution {
         }
     }
 }
+
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        //Since each element can only exist once in a permutation, we use a boolean array to keep record.
+        boolean[] used = new boolean[nums.length];
+        helper(list, 0, result, new ArrayList<>(), used);
+        return result;
+    }
+    
+    private void helper(List<Integer> nums, int index, List<List<Integer>> result, List<Integer> cur, boolean[] used) {
+        if (cur.size() == nums.size()) {
+            result.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (!used[i]) {
+                used[i] = true;
+                cur.add(nums.get(i));
+                helper(nums, index + 1, result, cur, used);
+                cur.remove(cur.size() - 1);
+                used[i] = false;
+            }
+            
+        }
+    }
+}
