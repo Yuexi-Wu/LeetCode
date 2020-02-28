cbb06db99c6a978557b4e0bc0396c7a7b9fbee7d
Yuexi-Wu <53302295+Yuexi-Wu@users.noreply.github.com>
2020-02-27 23:22:23 -0500

    Create 33. Search in Rotated Sorted Array

---
+++ BinarySearch/33SearchRot2.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        } else if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int left = 0; 
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            if (nums[left] < nums[mid]) {
+                //check whether in the ascending part
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        } else if (nums[right] == target) {
+            return right;
+        }
+        return -1;
+    }
+}
