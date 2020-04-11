47a8edf1f50b31dea790dd0b7832fde8b35a294c
Yuexi-Wu <53302295+Yuexi-Wu@users.noreply.github.com>
2020-04-11 15:56:24 -0400

    Update 72EditDistance.java

--- DynamicProgramming/72EditDistance.java
+++ DynamicProgramming/72EditDistance.java
@@ -15,9 +15,15 @@ class Solution {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    //the last character is same:
+                    //1 + Math.min(dp[i-1][j], dp[i][j-1]): remove/add
+                    //dp[i-1][j-1]: do nothing
                     dp[i][j] = 1 + Math.min(dp[i-1][j], dp[i][j-1]);
                     dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1]);
                 } else {
+                    //the last character is different:
+                    //1 + Math.min(dp[i-1][j], dp[i][j-1]):remove/add
+                    //dp[i-1][j-1]+1: replace
                     dp[i][j] = 1 + Math.min(dp[i-1][j], dp[i][j-1]);
                     dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1]+1);
                 }
