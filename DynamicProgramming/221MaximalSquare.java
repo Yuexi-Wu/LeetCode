770a9e996e2f01974d926a7400f0307d0bdd703d
Yuexi-Wu <53302295+Yuexi-Wu@users.noreply.github.com>
2020-04-28 14:05:27 -0400

    221. Maximal Square

---
+++ DynamicProgramming/221MaximalSquare.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        // Dynamic Programming
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dp = new int[n][m];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j] == '0' ? 0 : 1;
+                } else {
+                    if (matrix[i][j] == '1') {
+                        dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    } else {
+                        dp[i][j] = 0;
+                    }
+                }
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        return max*max;
+    }
+}
