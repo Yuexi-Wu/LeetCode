73cbbba8a6334651c054b91f19751dcf33c9304e
Yuexi-Wu <53302295+Yuexi-Wu@users.noreply.github.com>
2020-05-08 14:18:48 -0400

    Update CheckStraightLine.java

--- Array/CheckStraightLine.java
+++ Array/CheckStraightLine.java
@@ -4,21 +4,11 @@ class Solution {
         if (n == 2) {
             return true;
         }
-        int horizontal = -1, vertical = -1;
-        if (coordinates[1][0] == coordinates[0][0]) {
-            vertical = coordinates[1][0];
-        } else if (coordinates[1][1] == coordinates[0][1]) {
-            horizontal = coordinates[1][1];
-        }
         int x = coordinates[1][0] - coordinates[0][0];
         int y = coordinates[1][1] - coordinates[0][1];
         for (int i = 1; i < n-1; i++) {
-            if (vertical != -1) {
-                return coordinates[i+1][0] == vertical;
-            } else if (horizontal != -1) {
-                return coordinates[i+1][1] == horizontal;
-            } else {
-                return ((coordinates[i+1][0] - coordinates[i][0]) * y == (coordinates[i+1][1] - coordinates[i][1]) * x);
+            if ((coordinates[i+1][0] - coordinates[i][0]) * y != (coordinates[i+1][1] - coordinates[i][1]) * x) {
+                return false;
             }
         }
         return true;
